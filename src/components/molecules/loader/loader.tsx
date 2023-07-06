7b6168df2c76665758edfe29d83ab9e8999db602
guidovdijk <funnydutchguy@gmail.com>
2023-07-06 19:59:26 +0200

    refactor(Loader): Changed props of the LoaderSvg

--- src/components/molecules/loader/loader.tsx
+++ src/components/molecules/loader/loader.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react'
 
-const LoaderSvg: React.FC<any> = ({
-  color,
-  animation,
-  strokeOffsetArray,
-  ...props
-}) => (
-  <svg {...props} fill="none" viewBox="0 0 514 290" xmlns="http://www.w3.org/2000/svg">
-    <ellipse className={`-rotate-90 origin-center ${animation}`} cx="257" cy="145" rx="145" ry="257" stroke={color} strokeDasharray={1287.0380859375} strokeDashoffset={strokeOffsetArray} />
+const LoaderSvg: React.FC<any> = ({ className }) => (
+  <svg className="absolute" fill="none" viewBox="0 0 518 294" xmlns="http://www.w3.org/2000/svg">
+    <ellipse className="-rotate-90 origin-center absolute" cx="259" cy="147" rx="145" ry="257" stroke="#393939" />
+    <ellipse className={`-rotate-90 origin-center absolute z-10 ${className}`} cx="259" cy="147" rx="145" ry="257" stroke="#f1f1f1" strokeDasharray={1287.0380859375} strokeDashoffset={1287.0380859375} />
   </svg>
 )
 
@@ -20,9 +16,8 @@ export const Loader: React.FC<ILoaderProps> = ({
   children,
   isFinished,
 }) => (
-  <div className="flex items-center justify-center relative  w-[514px] h-[290px]">
+  <div className="flex items-center justify-center relative w-[514px] h-[290px]">
     <p className="text-callout text-center font-extralight">{children}</p>
-    <LoaderSvg animation="animation-dash" className="absolute z-10" color="#f1f1f1" strokeOffsetArray={1287.0380859375} />
-    <LoaderSvg className="absolute" color="#393939" />
+    <LoaderSvg className="animation-dash" />
   </div>
 )
