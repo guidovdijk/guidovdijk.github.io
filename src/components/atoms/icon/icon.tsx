e013a93f7ead5a36485833daf871637d1364d0db
guidovdijk <funnydutchguy@gmail.com>
2023-07-06 19:47:40 +0200

    feat(Icon): Added new icon components

--- src/components/atoms/icon/icon.tsx
+++ src/components/atoms/icon/icon.tsx
@@ -2,11 +2,17 @@ import * as React from 'react'
 import { Fullscreen } from './icons/Fullscreen'
 import { LinkedIn } from './icons/LinkedIn'
 import { Triangle } from './icons/Triangle'
+import { Instagram } from './icons/Instagram'
+import { Email } from './icons/Email'
+import { Github } from './icons/Github'
 
 const Components = {
   fullscreen: Fullscreen,
   linkedin: LinkedIn,
   triangle: Triangle,
+  instagram: Instagram,
+  email: Email,
+  github: Github,
 }
 
 export type IconName = keyof typeof Components
