d378c3de033dda2b5777aa68b09b12f5ad9d1d32
guidovdijk <funnydutchguy@gmail.com>
2023-07-06 20:00:01 +0200

    refactor(Link): Fixed className prop and middle aligned text

--- src/components/atoms/link/link.tsx
+++ src/components/atoms/link/link.tsx
@@ -9,10 +9,11 @@ export interface ILinkProps extends React.HTMLProps<HTMLAnchorElement> {
 export const Link: React.FC<ILinkProps> = ({
   iconName,
   children,
+  className = '',
   ...props
 }) => (
   <a
-    className="inline-flex text-white-200 text-body-s font-medium flex items-center gap-x-2 group hover:cursor-pointer"
+    className={`inline-flex align-middle text-white-200 text-body-s font-medium flex items-center gap-x-2 group hover:cursor-pointer ${className}`}
     {...props}
   >
     {iconName && <Icon name={iconName} />}
