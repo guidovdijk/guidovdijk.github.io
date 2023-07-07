6e6cef827eafc6cec2f82be85ab199e9cd717b79
guidovdijk <funnydutchguy@gmail.com>
2023-07-07 15:20:23 +0200

    refactor(Navigation): Changed storybook config for navigation component

--- src/components/organisms/navigation/navigation.stories.tsx
+++ src/components/organisms/navigation/navigation.stories.tsx
@@ -1,10 +1,17 @@
+import * as React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
 import { Navigation } from './index'
 
-export default {
+const meta: Meta = {
   component: Navigation,
-  title: 'Organisms/Navigation',
 }
 
-export const Default = {
-  args: {},
+export default meta
+type Story = StoryObj
+
+export const Default: Story = {
+  render: () => (
+    <Navigation />
+  ),
 }
