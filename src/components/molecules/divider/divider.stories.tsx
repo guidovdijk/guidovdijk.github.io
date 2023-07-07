df0242c8896f9653b8b3b1af10ce88465ba540a8
guidovdijk <funnydutchguy@gmail.com>
2023-07-07 17:02:56 +0200

    feat(Divider): WIP - Created divider component and animation

--- src/components/molecules/divider/divider.stories.tsx
+++ src/components/molecules/divider/divider.stories.tsx
@@ -1,10 +1,21 @@
+import * as React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
 import { Divider } from './index'
 
-export default {
+const meta: Meta = {
   component: Divider,
-  title: 'Molecules/Divider',
 }
 
-export const Default = {
-  args: {},
+export default meta
+type Story = StoryObj
+
+export const Default: Story = {
+  render: () => (
+    <>
+      <div className="h-screen" />
+      <Divider />
+      <div className="h-screen" />
+    </>
+  ),
 }
