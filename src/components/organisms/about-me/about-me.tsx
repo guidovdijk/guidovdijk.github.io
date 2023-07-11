5910d4fc99144152239a1130ce3adddaa833f057
guidovdijk <funnydutchguy@gmail.com>
2023-07-11 14:53:11 +0200

    feat(AboutMe): Added gradient backdrop to AboutMe

--- src/components/organisms/about-me/about-me.tsx
+++ src/components/organisms/about-me/about-me.tsx
@@ -3,6 +3,19 @@ import { Col, Grid } from '@/components/atoms/grid'
 import Image from 'next/image'
 import * as React from 'react'
 
+const AboutMeSvg: React.FC = () => (
+  <svg fill="none" viewBox="0 0 81 78" xmlns="http://www.w3.org/2000/svg">
+    <path d="M81 1H1V78" stroke="url(#paint0_linear_402_909)" />
+    <defs>
+      <linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_402_909" x1="41" x2="41" y1="1" y2="78">
+        <stop stopColor="#DD117E" />
+        <stop offset="0.489583" stopColor="#C079FE" />
+        <stop offset="1" stopColor="#9FD9FF" />
+      </linearGradient>
+    </defs>
+  </svg>
+)
+
 export const AboutMe: React.FC = () => (
   <Grid
     as="section"
@@ -25,7 +38,7 @@ export const AboutMe: React.FC = () => (
         </div>
       </div>
     </Col>
-    <Col className="col-span-12 sm:col-span-10 lg:col-span-4">
+    <Col className="col-span-12 relative gradient-shadow sm:col-span-10 lg:col-span-4">
       <Image
         alt="Image of Guido van Dijk, the developer of this portfolio"
         className="image-relative max-h-[660px] object-cover lg:object-fill"
@@ -33,6 +46,9 @@ export const AboutMe: React.FC = () => (
         src="images/profile.webp"
         style={{ objectPosition: 'top' }}
       />
+      <div className="absolute w-20 h-20 -top-3 -left-3">
+        <AboutMeSvg />
+      </div>
     </Col>
   </Grid>
 )
