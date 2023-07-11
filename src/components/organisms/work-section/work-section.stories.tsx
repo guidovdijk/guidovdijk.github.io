0bb04e47365a0f6a7efc6e7e9e47851cffcb1862
guidovdijk <funnydutchguy@gmail.com>
2023-07-11 16:30:20 +0200

    feat(WorkSection): Created WorkSection component

---
+++ src/components/organisms/work-section/work-section.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+import { WorkSection } from './index'
+
+const meta: Meta = {
+  component: WorkSection,
+}
+
+export default meta
+type Story = StoryObj
+
+export const Default: Story = {
+  render: () => (
+    <WorkSection />
+  ),
+}
