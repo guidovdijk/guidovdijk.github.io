a86b034e074b722d8897bf05fad7c14e6852224c
guidovdijk <funnydutchguy@gmail.com>
2023-07-11 14:07:48 +0200

    refactor(Grid): Removed container class from grid and made HTML element dynamic

--- src/components/atoms/grid/grid.tsx
+++ src/components/atoms/grid/grid.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
-import { HTMLProps } from 'react'
 
-export interface IGridProps extends HTMLProps<HTMLDivElement> {
-  cols?: number,
+export interface IGridProps extends React.HTMLAttributes<HTMLDivElement> {
+  cols?: number
+  className?: string
+  as?: React.ElementType
   children?: React.ReactNode
 }
 
 export const Grid: React.FC<IGridProps> = ({
   cols = 12,
+  as = 'div',
   children,
+  className,
   ...props
-}) => (
-  <div className={`container mx-auto px-12 grid auto-rows-max grid-cols-${cols} gap-6`} {...props}>{children}</div>
-)
+}) => {
+  const Component = as
+
+  return (
+    <Component className={`grid auto-rows-max grid-cols-${cols} gap-6 ${className}`} {...props}>{children}</Component>
+  )
+}
