35bf48b328c42b040aa45c3e413a6f037901c423
guidovdijk <funnydutchguy@gmail.com>
2023-07-11 20:47:08 +0200

    refactor(Button): Added remaining props and fixed spelling mistake

--- src/components/atoms/button/button.stories.tsx
+++ src/components/atoms/button/button.stories.tsx
@@ -24,6 +24,6 @@ export const Secondary: Story = {
 export const Teritairy: Story = {
   args: {
     ...Primary.args,
-    variation: 'teritary',
+    variation: 'tertiary',
   },
 }
--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 export interface IButtonProps {
   children: string,
-  variation: 'primary' | 'secondary' | 'teritary'
+  variation: 'primary' | 'secondary' | 'tertiary'
   onClick: () => void,
   className?: string
 }
@@ -10,15 +10,16 @@ export interface IButtonProps {
 const styles = {
   primary: 'border-gradient-background text-white-200 border border-solid py-4 px-12',
   secondary: 'text-white-200 divider-gradient-background',
-  teritary: 'bg-white-200/90 hover:bg-white-200 border border-white-200/75 py-3 px-12',
+  tertiary: 'bg-white-200/90 hover:bg-white-200 border border-white-200/75 py-3 px-12',
 }
 
 export const Button: React.FC<IButtonProps> = ({
   variation = 'primary',
   className,
+  children,
   ...props
 }) => (
-  <button className={`${styles[variation]} rounded text-body-s font-medium relative ${className}`} type="button">
-    {props.children}
+  <button {...props} className={`${styles[variation]} rounded text-body-s font-medium relative ${className}`} type="button">
+    {children}
   </button>
 )
