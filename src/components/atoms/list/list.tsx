49e5800bc05017ad0756119561a41440160eef75
guidovdijk <funnydutchguy@gmail.com>
2023-07-11 16:29:56 +0200

    feat(List): Added children option to list component

--- src/components/atoms/list/list.tsx
+++ src/components/atoms/list/list.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
 
 export interface IListProps {
-  title: string,
-  items: string[]
+  title?: string,
+  items?: string[]
   className?: string
+  children?: any
 }
 
 export const List: React.FC<IListProps> = ({
   title,
-  items,
+  items = null,
+  children = null,
   className = '',
   ...props
 }) => (
@@ -23,9 +25,16 @@ export const List: React.FC<IListProps> = ({
     }
     <ul className="items-center flex text-white-200" {...props}>
       {
-        items.map((item, i) => (
+        items && items.map((item, i) => (
           <li key={`card-${title}-${i}`} className="inline-flex items-center font-medium capitalize">
             {i > 0 && <span className="w-1 h-1 mx-4 bg-white-200 rounded-full" />}{item}
+          </li>
+        ))
+      }
+      {
+        children && React.Children.map(children, (child, i) => (
+          <li key={`card-${title}-${i}`} className="inline-flex items-center font-medium capitalize">
+            {i > 0 && <span className="w-1 h-1 mx-4 bg-white-200 rounded-full" />}{child}
           </li>
         ))
       }
