bc787591a6aeeb1e7e3501784db98e0df9085927
guidovdijk <funnydutchguy@gmail.com>
2023-07-12 15:17:09 +0200

    feat(useScroll): Created useScroll hook

---
+++ src/hooks/use-scroll/use-scroll.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useCallback, useState } from 'react'
+
+interface IUseScrollData {
+  x: number
+  y: number
+  lastX: number
+  lastY: number
+}
+
+const useScroll = (
+  handler: (data: IUseScrollData) => void,
+) => {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    lastX: 0,
+    lastY: 0,
+  })
+
+  const handleScroll = useCallback(
+    () => {
+      setData((last: { x: number; y: number }) => ({
+        x: window.scrollX,
+        y: window.scrollY,
+        lastX: last.x,
+        lastY: last.y,
+      }))
+
+      handler(data)
+    },
+    [data, handler],
+  )
+
+  useEffect(() => {
+    if (typeof window !== 'object') {
+      return undefined
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [handleScroll])
+
+  return data
+}
+
+export default useScroll
