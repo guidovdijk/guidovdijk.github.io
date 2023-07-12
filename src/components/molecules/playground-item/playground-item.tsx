eda14f8877818d2e09f4478d6402c9067de27216
guidovdijk <funnydutchguy@gmail.com>
2023-07-12 13:03:00 +0200

    refactor(PlaygroundItem): Added className to component

--- src/components/molecules/playground-item/playground-item.tsx
+++ src/components/molecules/playground-item/playground-item.tsx
@@ -5,16 +5,18 @@ import * as React from 'react'
 export interface IPlaygroundItemProps {
   image: ImageProps
   description: string
+  className?: string
   onClick: () => void
 }
 
 export const PlaygroundItem: React.FC<IPlaygroundItemProps> = ({
   image,
   description,
+  className = '',
   onClick,
   ...props
 }) => (
-  <article className="col-span-4 flex relative aspect-[9/14] overflow-hidden rounded border-16 transition-all group hover:border-none border-black-200" {...props}>
+  <article className={`col-span-4 w-full flex relative aspect-[9/14] overflow-hidden rounded border-16 transition-all group hover:border-none border-black-200 ${className}`} {...props}>
     <Image
       {...image}
     />
