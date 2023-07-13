2615cac1165fbab4903651253dcbec29384942b8
guidovdijk <funnydutchguy@gmail.com>
2023-07-13 17:08:40 +0200

    refactor(BentoGrid): Added className prop

--- src/components/organisms/bento-grid/bento-grid.tsx
+++ src/components/organisms/bento-grid/bento-grid.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import { Grid } from '@/components/atoms/grid'
 
-export const BentoGrid: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+export const BentoGrid: React.FC<{ className: string, children: React.ReactNode }> = ({
+  className,
+  children,
+}) => (
   <Grid
     as="section"
-    className="grid-rows-[repeat(9,120px)] grid-flow-dense gap-y-6"
+    className={`grid-rows-[repeat(9,120px)] grid-flow-dense gap-y-6 ${className}`}
   >
     {children}
   </Grid>
