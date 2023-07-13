af1fc1303a9264fb7710f2dd865b33d6a3da1904
guidovdijk <funnydutchguy@gmail.com>
2023-07-13 15:54:02 +0200

    refactor(Footer): Added padding to border

--- src/components/organisms/footer/footer.tsx
+++ src/components/organisms/footer/footer.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 
 export const Footer: React.FC = () => (
   <footer>
-    <div className="container mx-auto w-full bg-black-100 h-[1px]" />
+    <div className="container mx-auto w-full lg:px-12 bg-black-100 h-[1px]" />
     <Container className="px-12">
       <div className="flex flex-col gap-y-16 pt-16 pb-14">
         <div className="block">
