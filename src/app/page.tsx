a191bff781c78463559af9e335a2370eedf77f5b
guidovdijk <funnydutchguy@gmail.com>
2023-07-15 15:16:36 +0200

    feat(HomePage): Added HomeHero component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,18 @@ import { AboutMe } from '@/components/organisms/about-me'
 import { CTASection } from '@/components/organisms/cta-section'
 import { WorkSection } from '@/components/organisms/work-section'
 import { Container } from '@/components/atoms/grid'
+import { HomeHero } from '@/components/organisms/home-hero'
 
 export default function Home() {
   return (
-    <Container as="main" className="px-4 lg:px-12">
-      <div className="h-screen relative" />
-      <CTASection className="mb-28" />
-      <Divider className="mb-24" />
-      <WorkSection className="mb-44" />
-      <AboutMe className="mb-28" />
-    </Container>
+    <>
+      <HomeHero />
+      <Container as="main" className="px-4 lg:px-12">
+        <CTASection className="mb-28" />
+        <Divider className="mb-24" />
+        <WorkSection className="mb-44" />
+        <AboutMe className="mb-28" />
+      </Container>
+    </>
   )
 }
