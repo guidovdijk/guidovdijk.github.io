4d2a86013c6e86b2cd79fc19cbf44b9a2098883a
guidovdijk <funnydutchguy@gmail.com>
2023-07-17 17:53:00 +0200

    feat(404): Added scroll to top functionality

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
 'use client'
 
 import * as React from 'react'
+import { useEffect } from 'react'
 import { PageErrorSection } from '@/components/organisms/page-error-section'
 
 function NotFound() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <PageErrorSection error="404" title="Page not found" />
   )
