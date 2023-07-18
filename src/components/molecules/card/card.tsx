995b7b08e473c6bed6e6cb0f716f74fcd1393a86
guidovdijk <funnydutchguy@gmail.com>
2023-07-18 17:30:19 +0200

    feat(Card): Added fallback if no github link is provided and changed space between number and description

--- src/components/molecules/card/card.tsx
+++ src/components/molecules/card/card.tsx
@@ -30,13 +30,13 @@ export const Card: React.FC<ICardProps> = ({
     <article {...props} className={`rounded transition-all border-b border-black-200 lg:h-[118px] lg:hover:h-[148px] lg:pl-14 lg:pr-10 px-4 lg:pb-0 pb-4 group/card flex flex-col lg:flex-row lg:items-center ${className}`}>
       <NLink className="flex flex-col lg:flex-row flex-grow lg:items-center" href={href}>
         <div className="lg:flex shrink-0 gap-x-4 mb-6 lg:mb-0">
-          <span className="lg:group-hover/card:mr-14 font-normal transition-all lg:group-hover/card:text-heading-l">
+          <span className="xl:group-hover/card:mr-14 lg:group-hover/card:mr-4 font-normal transition-all lg:group-hover/card:text-heading-l">
             {`0${index}`}
           </span>
           <h3 className="lg:group-hover/card:hidden"><Image {...image} /></h3>
         </div>
         <p className="max-w-md lg:hidden group-hover/card:block mb-8 lg:mb-0">{description}</p>
-        <ul className="flex items-center ml-auto">
+        <ul className="flex shrink-0 items-center ml-auto">
           {
             subjects.map((subject, i) => (
               <li key={`card-${index}-${i}`} className="inline-flex items-center text-white-200 font-medium capitalize">
@@ -49,7 +49,18 @@ export const Card: React.FC<ICardProps> = ({
       <div className="lg:group-hover/card:py-5 h-full">
         <div className="bg-black-200 lg:group-hover/card:bg-white-200/20 md:mx-10 w-[1px] h-full" />
       </div>
-      <Link className="-mt-6 lg:mt-0 self-start lg:self-center" href={url}>GitHub</Link>
+      {
+        url
+          ? (
+            <Link
+              className="flex-row-reverse max-w-[50px] w-full -mt-6 lg:mt-0 self-start lg:self-center"
+              href={url}
+            >
+              GitHub
+            </Link>
+          )
+          : <span className="max-w-[50px] w-full text-center -mt-6 lg:mt-0 self-start lg:self-center">-</span>
+      }
     </article>
   )
 }
