103fd30c437703809dd51e6c9097d4b028dee4ac
guidovdijk <funnydutchguy@gmail.com>
2023-07-20 13:12:13 +0200

    feat(HomeHero): Added scroll animation to gradient image

--- src/components/organisms/home-hero/home-hero.tsx
+++ src/components/organisms/home-hero/home-hero.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import Image from 'next/image'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 import { HeroInfo } from '@/components/molecules/hero-info'
 import { Col, Container, Grid } from '@/components/atoms/grid'
@@ -14,37 +15,47 @@ export interface IHomeHero {
 
 export const HomeHero: React.FC<IHomeHero> = ({
   className = '',
-}) => (
-  <>
-    <div className="h-screen w-full absolute">
-      <HeroInfo />
-    </div>
-    <Container as="section" className={`px-4 lg:px-12 flex flex-col z-[1] min-h-screen overflow-hidden relative pt-20 ${className}`}>
-      <Grid className="mt-8 lg:mt-16 xl:mt-28 relative" gap="gap-0">
-        <Col span={12}>
-          <p className="pl-4 text-body">Hello There! I am...</p>
-        </Col>
-        <Col span={12}>
-          <h1 className="mb-4 lg:mb-14 font-heading font-bold uppercase text-heading-3-xl flex flex-col col-span-12">
-            <Grid as="span" gap="gap-0">
-              <Col as="span" className="block col-span-12">Guido</Col>
-              <Col as="span" className="block col-span-12">van Dijk</Col>
-              <Col as="span" className="block col-span-11 xl:col-span-10 lg:col-start-2 xl:col-start-3">Developer</Col>
-              <Col as="span" className="block col-span-11 xl:col-span-10 lg:col-start-4 xl:col-start-3">Designer</Col>
-            </Grid>
-          </h1>
-        </Col>
-        <Col className="absolute z-[-1] col-span-8 col-start-4 sm:col-span-6 sm:col-start-6 xl:col-start-7 xl:col-span-4">
-          <Image
-            alt="Gradient image"
-            className="top-0 max-h-[660px] w-full object-cover lg:object-fill"
-            src={GradientImage}
-          />
-        </Col>
-        <Col className="col-span-10 lg:col-span-12">
-          <LocationInfo>located in the netherlands</LocationInfo>
-        </Col>
-      </Grid>
-    </Container>
-  </>
-)
+}) => {
+  const { scrollYProgress } = useScroll()
+  const y = useTransform(scrollYProgress, [0, 1], ['0%', '-50%'])
+
+  return (
+    <>
+      <div className="h-screen w-full absolute">
+        <HeroInfo />
+      </div>
+      <Container as="section" className={`px-4 lg:px-12 flex flex-col z-[1] min-h-screen overflow-hidden relative pt-20 ${className}`}>
+        <Grid className="mt-8 lg:mt-16 xl:mt-28 relative" gap="gap-0">
+          <Col span={12}>
+            <p className="pl-4 text-body">Hello There! I am...</p>
+          </Col>
+          <Col span={12}>
+            <h1 className="mb-4 lg:mb-14 font-heading font-bold uppercase text-heading-3-xl flex flex-col col-span-12">
+              <Grid as="span" gap="gap-0">
+                <Col as="span" className="block col-span-12">Guido</Col>
+                <Col as="span" className="block col-span-12">van Dijk</Col>
+                <Col as="span" className="block col-span-11 xl:col-span-10 lg:col-start-2 xl:col-start-3">Developer</Col>
+                <Col as="span" className="block col-span-11 xl:col-span-10 lg:col-start-4 xl:col-start-3">Designer</Col>
+              </Grid>
+            </h1>
+          </Col>
+          <Col className="absolute z-[-1] col-span-8 col-start-4 sm:col-span-6 sm:col-start-6 xl:col-start-7 xl:col-span-4">
+            <motion.div
+              className="transform-gpu"
+              style={{ y }}
+            >
+              <Image
+                alt="Gradient image"
+                className="top-0 max-h-[660px] w-full object-cover lg:object-fill"
+                src={GradientImage}
+              />
+            </motion.div>
+          </Col>
+          <Col className="col-span-10 lg:col-span-12">
+            <LocationInfo>located in the netherlands</LocationInfo>
+          </Col>
+        </Grid>
+      </Container>
+    </>
+  )
+}
