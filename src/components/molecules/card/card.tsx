5773d93c8b7819709e2ca1d42c91b690c3ce8b2e
guidovdijk <funnydutchguy@gmail.com>
2023-07-21 14:11:05 +0200

    fix(Card): Anchor tag now takes the whole height

--- src/components/molecules/card/card.tsx
+++ src/components/molecules/card/card.tsx
@@ -1,7 +1,7 @@
-import { Link } from '@/components/atoms/link'
-import NLink from 'next/link'
-import Image, { ImageProps } from 'next/image'
 import * as React from 'react'
+import Image, { ImageProps } from 'next/image'
+import NLink from 'next/link'
+import { Link } from '@/components/atoms/link'
 
 export interface ICardProps {
   index: number
@@ -23,10 +23,10 @@ export const Card: React.FC<ICardProps> = ({
   className = '',
   ...props
 }) => (
-  <article {...props} className={`rounded transition-all border-b border-black-200 lg:h-[118px] lg:hover:h-[148px] lg:pl-14 lg:pr-10 px-4 lg:pb-0 pb-4 group/card flex flex-col lg:flex-row lg:items-center ${className}`}>
-    <NLink className="flex flex-col lg:flex-row flex-grow lg:items-center" href={href}>
+  <article {...props} className={`rounded transition-all border-b border-black-200 lg:pl-14 lg:pr-10 px-4 lg:pb-0 pb-4 group/card flex flex-col lg:flex-row lg:items-center ${className}`}>
+    <NLink className="flex flex-col lg:flex-row flex-grow transition-all lg:items-center lg:h-[118px] lg:group-hover/card:h-[148px]" href={href}>
       <div className="lg:flex shrink-0 gap-x-4 mb-6 lg:mb-0">
-        <span className="xl:group-hover/card:mr-14 lg:group-hover/card:mr-4 font-normal transition-all lg:group-hover/card:text-heading-l">
+        <span className="xl:group-hover/card:mr-14 lg:group-hover/card:mr-4 font-normal lg:group-hover/card:text-heading-l">
           {`0${index}`}
         </span>
         <h3 className="lg:group-hover/card:hidden"><Image {...image} /></h3>
@@ -42,8 +42,8 @@ export const Card: React.FC<ICardProps> = ({
         }
       </ul>
     </NLink>
-    <div className="lg:group-hover/card:py-5 h-full">
-      <div className="bg-black-200 lg:group-hover/card:bg-white-200/20 md:mx-10 w-[1px] h-full" />
+    <div className="h-full lg:h-[118px]">
+      <div className="bg-black-200 lg:group-hover/card:bg-white-200/20 md:mx-10 w-[0.5px] h-full" />
     </div>
     {
       url
