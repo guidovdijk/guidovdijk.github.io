ca1c6fa5bbda99629e96f383a64eae97847e6066
guidovdijk <funnydutchguy@gmail.com>
2023-07-22 12:55:23 +0200

    fix(Prettier): Fixed helper regex

--- prettier.config.js
+++ prettier.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '^react$',
     '^next/(.*)$',
     '<THIRD_PARTY_MODULES>',
-    '^@helpers/(.*)$',
+    '^@/helpers/(.*)$',
     '^@/hooks/(.*)$',
     '^@/components/(.*)$',
     '^[./]',
