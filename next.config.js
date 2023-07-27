5790ff2a63126e9bbc7a7e4740a991d359ced6bb
guidovdijk <funnydutchguy@gmail.com>
2023-07-27 16:29:52 +0200

    fix(Next): Updated config and workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === 'true',
 })
 
-
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  output: 'export',
+})
